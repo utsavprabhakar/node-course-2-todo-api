c2cac3954399e1a3c6185484f96043fad60a0b3b
utsavprabhakar <utsav.prabhakar@gmail.com>
2019-03-23 14:01:45 +0530

    Added PATCH todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash'); 
 var express= require('express');
 var app= express();
 var bodyParser= require('body-parser');
@@ -106,6 +107,30 @@ app.delete('/todos/:id', (req, res)=>{
     res.status(400).send();
 
   })
+})
+
+app.patch('/todos/:id', (req, res)=>{
+    var id = req.params.id;
+    var body = _.pick(req.body, ['text', 'completed']);
+    if(!ObjectId.isValid(id)){
+      res.status(404).send();
+    }
+    //console.log(body);
+    if(_.isBoolean(body.completed) && body.completed){
+      body.completedAt = new Date().getTime();
+    }else{
+      body.completed = false;
+      body.completedAt = null;
+    }
+    console.log(body);
+    Todo.findByIdAndUpdate(id, {$set : body}, {new : true}).then((todo)=>{
+      if(!todo){
+        return res.status(404).send();
+      }
+      res.send({todo});
+    }).catch((e)=>{
+      res.status(400).send();
+    })
 })
 
 app.get('/users', (req, res)=>{
